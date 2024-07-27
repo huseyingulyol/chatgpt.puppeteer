a832cfd11c7b5fb2be88cdd5d8813fac61050d1c
streamworlddev <streamworld15@gmail.com>
2024-07-27 15:33:24 +0300

    refactor

--- bot.js
+++ bot.js
@@ -5,9 +5,9 @@ require('dotenv').config();
   // Tarayıcıyı başlat
   const browser = await puppeteer.launch({
     headless: false,
-    executablePath: "C:/Program Files/Google/Chrome/Application/chrome.exe", // Chrome'un yolu
+    executablePath: process.env.BROWSER_LOCATION, // Chrome'un yolu
     args: [
-      "--user-data-dir=C:\\Users\\berka\\AppData\\Local\\Google\\Chrome\\User Data", // Profil dizininin yolu
+      `--user-data-dir=${process.env.BROWSER_USERDATA}`, // Profil dizininin yolu
       "--profile-directory=Default", // Varsayılan profil
       "--disable-blink-features=AutomationControlled", // Bu bayrak otomasyon kontrolünü devre dışı bırakır
     ],
@@ -22,12 +22,15 @@ require('dotenv').config();
   await pageChatGPT.setUserAgent(
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
   );
+
+  pagePercipio.bringToFront();
+  
   // İlk sekmede bir sayfaya git
   await pagePercipio.goto("https://tobeto.percipio.com/login#/classic");
 
   await pagePercipio.waitForSelector("input#loginName");
   await pagePercipio.click("input#loginName");
-  await pagePercipio.type("input#loginName", process.env.USERNAME);
+  await pagePercipio.type("input#loginName", process.env.PERCIPIO_EMAIL);
 
   await pagePercipio.waitForSelector(
     "button.Button---root---2BQqW.Button---primary---1O3lq.Button---small---3PMLN.Button---center---13Oaw"
@@ -37,7 +40,7 @@ require('dotenv').config();
   );
 
   await pagePercipio.waitForSelector('input[type="password"]');
-  await pagePercipio.type('input[type="password"]', process.env.PASSWORD);
+  await pagePercipio.type('input[type="password"]', process.env.PERCIPIO_PASSWORD);
 
   await pagePercipio.waitForSelector(
     'button.Button---root---2BQqW.Button---primary---1O3lq.Button---small---3PMLN.Button---center---13Oaw[type="submit"]'
@@ -47,10 +50,7 @@ require('dotenv').config();
   );
 
   // İkinci sekmede bir sayfaya git
-  // await pageChatGPT.goto("https://chatgpt.com");
-
-  // await pageChatGPT.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
-  // Yeni sekme açıldığında yeni sekmeye eriş
+  await pageChatGPT.goto("https://chatgpt.com");
 
   let previousUrl = ""; // Önceki URL'yi saklamak için bir değişken
   let isStarted = false;
@@ -247,217 +247,122 @@ require('dotenv').config();
 
           isStarted = true;
 
-          if (currentUrl.includes("/quizzes/")) {
-            while (isStarted) {
-              //SORUYU AL
-              let question = await newPage.evaluate(async (message) => {
-                //@@@@@@@@@@@@@@@@@@@@@@@@@BROWSER_CONTEXT@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+          if (currentUrl.includes("/quizzes/")) 
+          {
+  
+            while (isStarted) 
+            {
 
-                let isStartQuizSolver = false;
-                function delay(ms) {
-                  return new Promise((resolve) => setTimeout(resolve, ms));
-                }
+              //EĞER BOŞLUK DOLDURMAYSA ÇÖZ.
+              console.log("Blank işlemiyse yap.");
+              let isFillBlankQuestion = await newPage.evaluate(async () => {
 
-                async function checkElement(querySelectorText) {
-                  try {
-                    var element = document.querySelector(querySelectorText);
-                    if (element == null) {
-                      return false;
-                    } else {
-                      if (element.ariaDisabled == null) {
-                        if (element.disabled == undefined) {
-                          return true;
-                        } else {
-                          if (element.disabled == false)
-                            return true; // button aktif
-                          else return false;
-                        }
-                      } else {
-                        if (
-                          element.disabled == false &&
-                          element.ariaDisabled == "false"
-                        )
-                          return true; // aktifse
-                        else return false;
-                      }
-                    }
-                  } catch (error) {
-                    return false; // Hata durumunda boş string döndür
-                  }
-                }
+                function delay(ms) {return new Promise(resolve => setTimeout(resolve, ms));}
 
-                async function checkElementAndClick(querySelectorText) {
-                  while (!(await checkElement(querySelectorText))) {
-                    console.log("bekleniyor: " + querySelectorText);
-                    await delay(100);
-                  }
-                  await delay(100);
-                  document.querySelector(querySelectorText).click();
-                  console.log("bitti: " + querySelectorText);
-                }
+                const textContent = document.body.innerText || document.body.textContent;
+                var result = /fill/i.test(textContent) && /blank/i.test(textContent);
 
-                async function SolveQuestion() {
-                  try {
-                    const question = `
-                      Sana tüm sayfadaki metni veriyorum. Burda en başta bir soru ondan sonrada 4 şık var ve şıkların ıd'si var.
-                      Senden istediğim şu; soruyu çöz ve doğru olan cevabın id'sini döndür başka hiçbir şey döndürme!
-                
-                      Tüm sayfa metni:
-                      ${document.querySelector("main").textContent}
-                      Cevap şıkları:
-                      ${Array.from(
-                        document.querySelectorAll(
-                          'button[data-testid*="multiple-choice-answer"]'
-                        )
-                      )
-                        .map(
-                          (x) =>
-                            `\nID: ${x.getAttribute("data-testid")}\n${x.textContent}`
-                        )
-                        .join("\n")}
-                  `;
-                    return question;
-                  } catch (error) {
-                    console.log("Error:", error);
-                  }
-                }
+                if (result)
+                {
+                  const elements = document.querySelectorAll("button[data-flip-id*='correct']");
 
-                async function StartQuizSolver() {
-                  isStartQuizSolver = true;
-                  await SolveQuestion();
-                }
-
-                async function StopQuizSolver() {
-                  isStartQuizSolver = false;
-                }
-
-                const button = document.createElement("button");
-                button.id = "dynamic-start-button"; // Butona id atayarak kontrolü kolaylaştır
-                button.textContent = "START QUIZ SOLVER";
-                button.style.position = "fixed";
-                button.style.bottom = "20px";
-                button.style.right = "20px";
-                button.style.padding = "10px 20px";
-                button.style.backgroundColor = "green"; // Buton rengi
-                button.style.color = "white";
-                button.style.border = "none";
-                button.style.borderRadius = "5px";
-                button.style.cursor = "pointer";
-                button.style.zIndex = "1000"; // Diğer öğelerin üstünde görünsün
-                document.body.appendChild(button);
-
-                button.addEventListener("click", async () => {
-                  if (!isStartQuizSolver) {
-                    button.style.backgroundColor = "red";
-                    button.textContent = "STOP QUIZ SOLVER";
-                    await StartQuizSolver();
-                  } else {
-                    button.style.backgroundColor = "green";
-                    button.textContent = "START QUIZ SOLVER";
-                    await StopQuizSolver();
-                  }
-                });
-
-                if (message) {
-                  button.click();
-                }
-
-                //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-              }, isStarted);
-
-              pageChatGPT.bringToFront();
-
-              //CEVABI ÖĞREN
-              let answer = await pageChatGPT.evaluate(async (message) => {
-                //@@@@@@@@@@@@@@@@@@@@@@@@@PAGE_CHATGPT_BROWSER_CONTEXT@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-                function delay(ms) {
-                  return new Promise((resolve) => setTimeout(resolve, ms));
-                }
-
-                async function GetAnswer() {
-                  await delay(1000);
-                  const input = document.querySelector(
-                    'textarea[id*="prompt-textarea"]'
-                  );
-                  input.focus();
-                  const event = new Event("input", { bubbles: true });
-                  input.value = message;
-                  input.dispatchEvent(event);
-                  await delay(500);
-
-                  document
-                    .querySelector("button[data-testid*='send-button']")
-                    .click();
-                  await delay(2000);
-
-                  let answerChatGPT = document.querySelectorAll(
-                    "div[data-message-author-role='assistant']"
-                  )[
-                    document.querySelectorAll(
-                      "div[data-message-author-role='assistant']"
-                    ).length - 1
-                  ].textContent;
-
-                  await delay(500);
-
-                  return answerChatGPT;
-                }
-
-                await GetAnswer();
-
-                //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-              }, question);
-
-              newPage.bringToFront();
-
-              // CEVABI İŞARETLE
-              await newPage.evaluate(async (message) => {
-                //@@@@@@@@@@@@@@@@@@@@@@@@@CODEACADEMY_BROWSER_CONTEXT@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
-                async function ClickAnswer() {
-                  let currentCounter = document.querySelector(
-                    "span[aria-label*='Question']"
-                  ).textContent;
-                  console.log("Counter'a erişildi!");
-                  console.log(currentCounter);
-
-                  console.log("Gelen cevap:", message);
-                  await checkElementAndClick(
-                    `button[data-testid*="${message.trim()}"]`
-                  );
-                  console.log("cevaba tıklandı!");
-                  await delay(1000);
-                  await checkElementAndClick(
-                    'button[data-testid*="quiz-button-cta"]'
-                  );
-                  await delay(300);
-                  console.log("next'e tıklandı!");
-                  await delay(1000);
-
-                  await delay(200);
-                  while (
-                    !(await checkElement("span[aria-label*='Question']"))
-                  ) {
-                    await delay(200);
-                    console.log("Counter bekleniyor...");
-                  }
-
-                  while (
-                    currentCounter ==
-                    document.querySelector("span[aria-label*='Question']")
-                      .textContent
-                  ) {
-                    console.log("Counter aynı bulundu bekleniyor...");
+                  // Elementleri ID'nin son hanesine göre sırala
+                  const sortedElements = Array.from(elements).sort((a, b) => {
+                      const idA = a.getAttribute('data-flip-id');
+                      const idB = b.getAttribute('data-flip-id');
+                      
+                      const lastDigitA = parseInt(idA.slice(-1), 10);
+                      const lastDigitB = parseInt(idB.slice(-1), 10);
+                      
+                      return lastDigitA - lastDigitB;
+                  });
+                  
+                  sortedElements.forEach(async (element, index) => {
                     await delay(1000);
-                  }
-
-                  console.log("Counter değişti işlem başarılı!");
+                    element.click(); // Elementi tıklat
+                    console.log(`Element ${index + 1} tıklatıldı:`, element);
+                  });
                 }
 
-                await ClickAnswer();
+                return result;
+              });
 
-                //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-              }, answer);
+              console.log("Blank mi?");
+              if (!isFillBlankQuestion)
+              {
+                console.log("Blank değil, normal soru muamelisine başladım.");
+                //Butonların yüklenmesini bekle.
+                await newPage.waitForFunction(() => document.querySelectorAll("button[data-testid*='multiple-choice-answer']").length == 4);
+                console.log("Tüm butonlar yüklendi.");
+
+                //SORUYU AL
+                let question = await newPage.evaluate(() => {
+                  return `
+                        Sana tüm sayfadaki metni veriyorum. Burda en başta bir soru ondan sonrada 4 şık var ve şıkların ıd'si var.
+                        Senden istediğim şu; soruyu çöz ve doğru olan cevabın id'sini döndür başka hiçbir şey döndürme!
+                  
+                        Tüm sayfa metni:
+                        ${document.querySelector("main").textContent}
+                        Cevap şıkları:
+                        ${Array.from(
+                          document.querySelectorAll(
+                            'button[data-testid*="multiple-choice-answer"]'
+                          )
+                        )
+                          .map(
+                            (x) =>
+                              `\nID: ${x.getAttribute("data-testid")}\n${x.textContent}`
+                          )
+                          .join("\n")}
+                    `;
+                });
+              
+                //CEVABI ÖĞREN
+                pageChatGPT.bringToFront();
+                let answer = await pageChatGPT.evaluate(async (message) => {
+                  //@@@@@@@@@@@@@@@@@@@@@@@@@PAGE_CHATGPT_BROWSER_CONTEXT@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+                  function delay(ms) {
+                    return new Promise((resolve) => setTimeout(resolve, ms));
+                  }
+
+                  async function GetAnswer() {
+                    await delay(1000);
+                    const input = document.querySelector('textarea[id*="prompt-textarea"]');
+                    input.focus();
+                    const event = new Event("input", { bubbles: true });
+                    input.value = message;
+                    input.dispatchEvent(event);
+                    await delay(500);
+
+                    document.querySelector("button[data-testid*='send-button']").click();
+                    await delay(2000);
+
+                    let answerChatGPT = document.querySelectorAll("div[data-message-author-role='assistant']")
+                    [document.querySelectorAll("div[data-message-author-role='assistant']").length - 1].textContent;
+
+                    await delay(500);
+                    console.log(answerChatGPT);
+
+                    return answerChatGPT;
+                  }
+
+                  await GetAnswer();
+
+                  //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+                }, question);
+
+                // CEVABI İŞARETLE
+                console.log("Cevabı işaretliyorum. cevap: ", answer);
+                newPage.bringToFront();
+                await newPage.click(`button[data-testid*="${answer.trim()}"]`);
+                
+
+              }
+
+              // NEXT TUŞUNA BAS
+              console.log("Next'e basıyorum.");
+              await newPage.waitForTimeout(500);
+              await newPage.click('button[data-testid*="quiz-button-cta"]');
             }
           }
         });
